18ee378a83636fab96a2b7ab3302837cf355bc67
Murad <murad.ragimow2902@gmail.com>
2019-01-21 01:44:24 +0400

    Added ViewModels for city

--- TripApp/ViewModels/MenuViewModel.cs
+++ TripApp/ViewModels/MenuViewModel.cs
@@ -13,7 +13,6 @@ namespace TripApp.ViewModels
 {
     class MenuViewModel : ViewModelBase
     {
-
         private readonly INavigationService navigation;
 
         public MenuViewModel(INavigationService navigation)
